790d763f6e13850e9f8be09e0d8c22ac373900d2
juanveraj <90976132+juanveraj@users.noreply.github.com>
2021-10-13 19:23:53 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,36 @@
+import os
+from flask import flash, render_template
+from flask.app import Flask
+
+app =Flask(__name__)
+app.secret_key = os.urandom(24)
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/registro')
+def registro():
+    return render_template("registro.html")
+
+@app.route('/buscarcita')
+def buscarcita():
+    return render_template("buscarcita.html")
+
+@app.route('/mostrarcita')
+def mostrarcita():
+    return render_template("mostrarcita.html")
+
+
+@app.route('/perfil')
+def verperfil():
+    return render_template("perfil.html")
+
+@app.route('/editarperfil')
+def editarperfil():
+    return render_template("editar_perfil.html")
+
+
+
+
